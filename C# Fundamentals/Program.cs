f8833379ae79267cd9672e2b783800137c86f56c
elvisonob <elvisonob@yahoo.com>
2024-05-13 18:59:44 +0100

    Add project files.

---
+++ C# Fundamentals/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = 10;
+int number1 = 5;
+
+Console.WriteLine("Addition:" + (number + number1));
+Console.WriteLine("Subtraction:" + (number - number1));
+Console.WriteLine("Division:" + number / number1);
+Console.WriteLine("Multiplication:" + number * number1);
+Console.WriteLine("John" + "            " + "Smith");
+
+
+
