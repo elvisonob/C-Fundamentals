bc7aa0da23ed993808d4c5b95d439eb3108944cf
elvisonob <elvisonob@yahoo.com>
2024-05-17 09:48:09 +0100

    git commit else if block

--- C# Fundamentals/Program.cs
+++ C# Fundamentals/Program.cs
@@ -2,13 +2,23 @@
 Console.WriteLine("Hello!");
 Console.WriteLine("What do you want to do");
 
+var userChoice = Console.ReadLine();
 
-string elvis = Console.ReadLine();
-Console.WriteLine("User input:" + elvis);
-elvis = "ABC";
-
-var result = "abc" + "def" + "ghi";
+if (userChoice.Length <= 3) 
+{
+    Console.WriteLine("short answer");
+}  if (userChoice.Length<10)
+{
+    Console.WriteLine("longer than three");
+} else
+{
+    Console.WriteLine("longer ooo");
+}
 
 Console.ReadKey();
+
+
+
+
 
 
